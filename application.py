9bc9439ba338d3134270da7088cd837010a4b0b8
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2019-05-15 18:25:34 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,3 @@
-import socket
-import spacy
-import base64
-from urllib.request import urlopen
-
-
-pretrainedmodel = spacy.load("en_core_web_sm")
-
-
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
