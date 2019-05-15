09159a960ee7096d85b1ca205ec446de08dd5de6
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2019-05-15 19:49:05 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,9 +10,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    documentContents = request.args.get('contents')
-    doc2 = pretrainedmodel(documentContents)
-    JSONOut = "{\"contents\" : \"" + str(base64.b64encode(bytes(documentContents,"utf-8"))).replace("b'","") + "\", \"ner\" : ["
-    for ent in doc2.ents:
-       JSONOut += "{\"label\" : \"" + ent.label_ + "\", \"text\" : \"" + str(base64.b64encode(bytes(ent.text,"utf-8"))).replace("b'","") + "\",\"start\" : \"" + str(ent.start_char) + "\",\"end\" : \"" + str(ent.end_char) + "\"},"
-    return JSONOut
+    try:
+        documentContents = request.args.get('contents')
+        doc2 = pretrainedmodel(documentContents)
+        JSONOut = "{\"contents\" : \"" + str(base64.b64encode(bytes(documentContents,"utf-8"))).replace("b'","") + "\", \"ner\" : ["
+        for ent in doc2.ents:
+           JSONOut += "{\"label\" : \"" + ent.label_ + "\", \"text\" : \"" + str(base64.b64encode(bytes(ent.text,"utf-8"))).replace("b'","") + "\",\"start\" : \"" + str(ent.start_char) + "\",\"end\" : \"" + str(ent.end_char) + "\"},"
+        return JSONOut
+    except Exception as e:
+        error=str(e)
+        return error
+        
