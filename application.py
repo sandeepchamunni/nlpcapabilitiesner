39f51b78d0de49b6413e89758c82d450c6f8d642
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2019-05-15 19:44:37 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,4 +10,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    documentContents = request.args.get('contents')
+    doc2 = pretrainedmodel(documentContents)
+    JSONOut = "{\"contents\" : \"" + str(base64.b64encode(bytes(documentContents,"utf-8"))).replace("b'","") + "\", \"ner\" : ["
+    for ent in doc2.ents:
+       JSONOut += "{\"label\" : \"" + ent.label_ + "\", \"text\" : \"" + str(base64.b64encode(bytes(ent.text,"utf-8"))).replace("b'","") + "\",\"start\" : \"" + str(ent.start_char) + "\",\"end\" : \"" + str(ent.end_char) + "\"},"
+    return JSONOut
