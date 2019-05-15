f1def2128863c0d12b2f9a2f47cc76de086bf3ce
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2019-05-15 19:41:48 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,6 +3,9 @@ import spacy
 import base64
 from urllib.request import urlopen
 #from urllib.request import urlopen
+
+pretrainedmodel = spacy.load("en_core_web_sm")
+
 app = Flask(__name__)
 
 @app.route("/")
